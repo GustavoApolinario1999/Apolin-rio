a4393bd7171c044541f1f384191cdb6f75c469c9
gustavo.ansantos <gustavo_apo@outlook.com>
2018-09-20 20:16:19 -0300

    Teste 1

--- Gustavo/Gustavo.py
+++ Gustavo/Gustavo.py
@@ -1,1 +1,16 @@
-print("ola mundo")
+import math
+
+a = int(input("Informe valor de a: "))
+b = int(input("Informe valor de b: "))
+c = int(input("Informe valor de c: "))
+
+delta = b * b - 4 * a * c
+print("delta: ", delta)
+
+if delta >= 0:
+    x1 = (-b + math.sqrt((delta))) / (2 * a)
+    x2 = (-b + math.sqrt((delta))) / (2 * a)
+    print("Raiz x1: ", x1)
+    print("Raiz x2: ", x2)
+else:
+    print("Não existem raizes reais")
